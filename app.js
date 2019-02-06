e78666a6483acb755557430a48469dde7e709643
david <fuzzydunlop227@gmail.com>
2019-02-06 10:25:34 +0000

    Removed URL

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ var express     = require("express"),
 var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
- console.log(process.env.DATABASEURL);
+    
 var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp";
 mongoose.connect(url);
-// mongoose.connect("mongodb://dave:fakepass12@ds123635.mlab.com:23635/fakecamp");
+
 
 
 app.use(bodyParser.urlencoded({extended: true}));
